583510571233f3398578eb1bc94fc1addeab0863
Nguyen <Kiet.Nguyen15@hanes.com>
2024-05-09 20:37:46 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = (props) => {
     }, [isStart]);
 
     const _onDetected = res => {
-        stopScanner();
+        // stopScanner();
         console.log(res.codeResult.code)
         setBarcode(res.codeResult.code);
     };
@@ -74,7 +74,19 @@ const App = (props) => {
                     }
                 },
                 decoder: {
-                    readers: props.readers
+                    readers: [
+                        'code_128_reader',
+                        'ean_reader',
+                        'ean_8_reader',
+                        'code_39_reader',
+                        'code_39_vin_reader',
+                        'codabar_reader',
+                        'upc_reader',
+                        'upc_e_reader',
+                        'i2of5_reader',
+                        '2of5_reader',
+                        'code_93_reader',
+                    ]
                 }
             },
             err => {
